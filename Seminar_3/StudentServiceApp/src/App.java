8c1031dc312e195addd87c4c542931e2e48639cb
ples <plescov01@gmail.com>
2023-06-29 20:09:35 +0300

    Семинар_3 - ДЗ (задача со *) 4. Отсортировать группы студентов в потоке сначало по количеству студентов, а затем по идентификатору группы

--- Seminar_3/StudentServiceApp/src/App.java
+++ Seminar_3/StudentServiceApp/src/App.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import StudentDomen.Student;
@@ -18,14 +19,15 @@ public class App {
         students1.add(new Student("Владислав", "Наджахов", 23, 102));
         students1.add(new Student("Леша", "Сидоров", 20, 106));
         students1.add(new Student("Михаил", "Петров", 19, 103));
-        StudentGroup group1 = new StudentGroup(students1);
+        students1.add(new Student("Артем", "Петров", 19, 103));
+        StudentGroup group1 = new StudentGroup(students1, 1);
         studentGroups.add(group1);
         
         //Создание группы 2
         List<Student> students2 = new ArrayList<>();
         students2.add(new Student("Александр", "Петрович", 20, 109));
         students2.add(new Student("Даша", "Телепнева", 21, 104));
-        StudentGroup group2 = new StudentGroup(students2);
+        StudentGroup group2 = new StudentGroup(students2, 2);
         studentGroups.add(group2);
         
         //Создание группы 3
@@ -35,7 +37,7 @@ public class App {
         students3.add(new Student("Денис", "Морозов", 22, 120));
         students3.add(new Student("Наталья", "Козлова", 23, 173));
         students3.add(new Student("Игорь", "Волков", 23, 150));
-        StudentGroup group3 = new StudentGroup(students3);
+        StudentGroup group3 = new StudentGroup(students3, 3);
         studentGroups.add(group3);
 
         //Создание группы 4
@@ -44,18 +46,18 @@ public class App {
         students4.add(new Student("Екатерина", "Смирнова", 21, 101));
         students4.add(new Student("Иван", "Попов", 20, 113));
         students4.add(new Student("Ольга", "Соколва", 22, 110));
-        StudentGroup group4 = new StudentGroup(students4);
+        StudentGroup group4 = new StudentGroup(students4, 4);
         studentGroups.add(group4);
 
         // Создание потока студентов
         StudentStream studentStream = new StudentStream(1, studentGroups);
 
-        // Сортировка групп студентов по количеству студентов
+        // Сортировка групп студентов по количеству студентов и идентификатору группы
         Collections.sort(studentStream.getStudentGroups());
 
         // Вывод групп со списками стдентов
         for (StudentGroup group : studentStream) {
-            System.out.println("Группа: ");
+            System.out.println("Группа " + group.getGroupID() + ":");
             for (Student student : group.getStudents()) {
                 System.out.println(student);                
             }
--- Seminar_3/StudentServiceApp/src/StudentDomen/StudentGroup.java
+++ Seminar_3/StudentServiceApp/src/StudentDomen/StudentGroup.java
@@ -3,28 +3,43 @@ package StudentDomen;
 import java.util.List;
 
 public class StudentGroup implements Comparable<StudentGroup> {
+    
+    
     private List<Student> students;
-
-    public StudentGroup(List<Student> students) {
+    private int groupID;    // Идентификатор группы
+    
+    public StudentGroup(List<Student> students, int groupID) {
         this.students = students;
+        this.groupID = groupID;
     }
-
+    
     public List<Student> getStudents() {
         return students;
     }
-
+    
     public void setStudents(List<Student> students) {
         this.students = students;
     }
     
+    public int getGroupID() {
+        return groupID;
+    }
+    
+    public void setGroupID(int groupID) {
+        this.groupID = groupID;
+    }
+    
     @Override
     public String toString() {
-        return "StudentGroup [students=" + students.toString() + "]";
+        return "StudentGroup [groupID=" + groupID + ", students=" + students + "]";
     }
 
     @Override
     public int compareTo(StudentGroup o) {
-        return Integer.compare(this.students.size(), o.students.size());
+        int studentCountComparison = Integer.compare(this.students.size(), o.students.size());
+        if (studentCountComparison != 0) {
+            return studentCountComparison;
+        }
+        return Integer.compare(this.groupID, o.groupID);
     }
-    
 }
